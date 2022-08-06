e67b79dca3688ce6eed8809cc99e6009983c9cc5
dhaivy <dhaivion@outlook.com>
2022-08-06 17:19:01 -0500

    added and configured tailwind css

---
+++ client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+// This is the template path for Tailwind. (What is a template path?)
+
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
